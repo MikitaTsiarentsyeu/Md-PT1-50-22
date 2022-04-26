537da4b6371a89348886200a9c5b1309483722e3
Mikita Tsiarentsyeu <Mikita_Tsiarentsyeu@epam.com>
2022-04-26 19:48:55 +0600

    add lesson 26.04

---
+++ Lessons/lesson 26.04/mth.py
@@ -0,0 +1,12 @@
+import math
+import random
+
+print(math.pi, type(math.pi))
+print(math.e, type(math.e))
+
+print(math.sin(math.pi), math.cos(math.pi))
+
+print(math.factorial(10))
+
+print(random.randint(0,10))
+print(random.choice([1,2,3,4,5,6,7,8,9,10]))
---
+++ Lessons/lesson 26.04/numbers.py
@@ -0,0 +1,40 @@
+# from decimal import Decimal
+# import fractions
+
+from sympy import frac
+
+print(type(30000000000000000000000000000000000000000000000000000))
+
+print(6+2)
+print(6-2)
+print(6*2)
+print(6/2)
+print(6//2)
+
+x = int(2.8)
+
+print(type(x), x)
+
+x = int(-2.8)
+
+print(type(x), x)
+
+x = int('3')
+
+print(type(x), x)
+
+print(type(3.5))
+
+x = 3 + 5.4
+print(x, type(x))
+
+x = float('3.6')
+print(x, type(x))
+
+print(1.1 + 2.2 == 3.3)
+
+
+# x = Decimal(0.1)
+# print(x, type(x))
+
+# print(fractions.Fraction(2.5))
