7a33654dccbd8a9ea7e469cf8a28e838ddb99b25
AnnaRybina <anna.zm@tut.by>
2022-05-04 22:07:16 +0300

    Update Task1

--- Tasks/Rybina/Task1/Task1.py
+++ Tasks/Rybina/Task1/Task1.py
@@ -1,15 +1,9 @@
-<<<<<<< Updated upstream
-=======
 #variant1
 
->>>>>>> Stashed changes
 deposit = 20000
 period = 60
 rate = 15/12/100
 x = deposit*(rate + 1)**period
-<<<<<<< Updated upstream
-print(x)
-=======
 print(x)
 
 #variant2
@@ -20,4 +14,4 @@ rate = 15/12/100
 for period in range(60):
     deposit = deposit*(rate+1)
 print(deposit)
->>>>>>> Stashed changes
+    
