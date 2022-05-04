617bfb3fddb6e87f7cb0d70ab88fb0ef288e8834
IvanChuro <104319589+IvanChuro@users.noreply.github.com>
2022-05-04 21:27:29 +0300

    Delete add_tasks1.py

--- Tasks/Churo/add_tasks1.py
+++
@@ -1,5 +0,0 @@
-initial_amount = 20000
-term = 5
-percent = 15
-total_amount = initial_amount * (1 + ((percent / 100) / 12)) ** (term * 12)
-print("Total amount after 5 years", round(total_amount, 2), "BYN")
