12012a847d39b4a3dd7d7625441d4e99cf34b915
Yauheni Buhaev <buhaev.yauheni@gmail.com>
2022-05-05 19:27:09 +0300

    Update Task1.py

--- Tasks/Buhayev/Task1/Task1.py
+++ Tasks/Buhayev/Task1/Task1.py
@@ -14,49 +14,72 @@ print(lambda_total_deposit())
 
 #function with some simple logic:
 def input_initial(init, sub_str = ""):
-	try:
-		initial_var = int(input(f"Enter your {init} {sub_str}: "))
-		return initial_var
-	except:
-		print('Sorry, wrong value, please try again')
-		return input_initial(init)
+   
+    try:
+        initial_var = int(input(f"Enter your {init} {sub_str}:\n"))
+        return initial_var
+    except:
+        print('Sorry, wrong value, please try again')
+        return input_initial(init)
 		
 def input_time_unit():
-	time_unit = input('What kind of time unites do you prefer (year, month, day)?  ').lower() 
-	if time_unit in ["year", "month", "day"]:
-		return time_unit
-	else:
-		print("invalid unit, please try again")
-		return input_time_unit()
-	   	
-def calculation_deposit(amount, duration, percent, factor):
-    total = round((amount * (1 + percent / 100 / factor) ** (duration * factor)), 2)
-    diff = round((total - amount), 2)
-    print("Your total deposit amount after {} years is: {}\n total interest: {}".format(duration, total, diff))
+   
+    time_unit = input('Enter kind of time unites do you prefer (year, month, day):\n').lower() 
+    if time_unit in ["year", "month", "day"]:
+        return time_unit
+    else:
+        print("invalid unit, please try again")
+        return input_time_unit()
     
 
+def calculate_second_operand(duration, percent, factor): 
+    	   	return (1 + percent / 100 / factor) ** (duration * factor)
+     
+     
+     
+def calculation_deposit(amount, degree):
+    total = round((amount * degree), 2)
+    return total
+
+def calculate_difference(amount, total): 
+    return round((total - amount),2)
+
+
+
+def print_answer(total, diff, duration): 
+    print(f"Your total deposit amount after {duration} years is: {total}\n total interest: {diff}")
+
+
+
+def calculation_depence_unit(time_unit, amount, duration, percent):
+    if time_unit == "year":
+        second_arg_for_total = calculate_second_operand(duration, percent, factor=1)
+        total = calculation_deposit(amount, second_arg_for_total)
+        diff = calculate_difference(amount, total)
+        print_answer(total, diff, duration)
+    elif time_unit == "month":
+        second_arg_for_total = calculate_second_operand(duration, percent, factor=12)
+        total = calculation_deposit(amount, second_arg_for_total)
+        diff = calculate_difference(amount, total)
+        print_answer(total, diff, duration)
+    elif time_unit == "day":
+        second_arg_for_total = calculate_second_operand(duration, percent, factor=365)
+        total = calculation_deposit(amount, second_arg_for_total)
+        diff = calculate_difference(amount, total)
+        print_answer(total, diff, duration)
+    else:
+        print("Sorry, we can't calculate this =(")
+
+
 def main():
+    
     amount = input_initial('amount')
     duration = input_initial('duration', '(int in years)')
     time_unit = input_time_unit()
     percent = input_initial("percent")
-    if time_unit == "year":
-        factor = 1
-        calculation_deposit(amount, duration, percent, factor)
-    elif time_unit == "month":
-        factor = 12
-        calculation_deposit(amount, duration, percent, factor)
-    elif time_unit == "day":
-        factor = 365
-        calculation_deposit(amount, duration, percent, factor)
-    else:
-        print("Sorry, we cant calculate this =(")
-        
+    calculation_depence_unit(time_unit, amount, duration, percent)
     continue_calulations =  input("Do you want to calculate another deposit (yes or no): ").lower()
-        
-    main() if continue_calulations in  ["y" , "yes", "yep", "ya", "ye"] else None
+    main() if continue_calulations in  ["yes" , "y", "yep", "ya", "ye"] else None
         	
- 
-        
 if __name__ == "__main__":    
     main()
