19a274f1058b82d2ab6cb5a4024d50647c3f7d7b
1161607 <1161607@gmail.com>
2022-05-16 12:58:03 +0300

    corrected task1

--- Tasks/Yourasov/Task1/DepositCalculator.py
+++ Tasks/Yourasov/Task1/DepositCalculator.py
@@ -1,7 +1,9 @@
-amount = float(input('Enter deposit amount: '))
-term = float(input('Enter deposit term, years: '))
-rate = float(input('Enter deposit interest rate in %: '))
-total_sum = round(amount * (1 + rate / 100 / 12)**(term * 12), 2)
-total_rate_sum = round(total_sum - amount, 2)
+import decimal
+
+amount = decimal.Decimal(input('Enter deposit amount: '))
+term = decimal.Decimal(input('Enter deposit term, years: '))
+rate = decimal.Decimal(input('Enter deposit interest rate in %: '))
+total_sum = amount * (1 + rate / 100 / 12)**(term * 12)
+total_rate_sum = total_sum - amount
 print('Total deposit amount =', '{:,.2f}'.format(total_sum).replace(',', ' '), end=', ')
 print('included amount of interest =', '{:,.2f}'.format(total_rate_sum).replace(',', ' '))
