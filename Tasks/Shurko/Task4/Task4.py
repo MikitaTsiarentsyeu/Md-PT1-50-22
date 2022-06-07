19c42a48742929960f9f679ed2a98e182fef92bd
ANDREI SHURKO <104149538+AndreiCDI@users.noreply.github.com>
2022-06-07 13:53:45 +0300

    Update  Task4

--- Tasks/Shurko/Task4/Task4.py
+++ Tasks/Shurko/Task4/Task4.py
@@ -18,33 +18,37 @@ def is_prime(num: int):
     
 #print(is_prime(11))
 
-list_ = [0, 1, 2, 3, 4, 7, 8, 10]
+#list_ = [0, 1, 2, 3, 4, 7, 8, 10]
 
 
 
 
 l = [0, 1, 2, 3, 4, 7, 8, 10]
-l = [4,7,10]
+#l = [4,7,10]
 #l = [2, 3, 8, 9]
-
 def get_range(l: list):  # This part of task4 in progress
     my_srt = ""
     new = list()
     new.append(l[0])
     i = 1
     j = 0
+    change = False
     while i < len(l):
         if (l[i]-1) == l[i-1]:
             new[j+1:j+3] =[l[i]]
-        
-        else: 
-            new.append(l[i])
+            change = True
+        elif change == True: 
+            new.insert(j+1, '-')
+            change = False
             j +=2
+            i -=1
+        else:
+            new.append(l[i])
+            j +=1
         i +=1
+    if change == True:
+        new.insert(j+1, '-')
+    print(new)
     my_srt = [str(n) for n in new]
     return my_srt
 print(get_range(l))
-
-
-
-
