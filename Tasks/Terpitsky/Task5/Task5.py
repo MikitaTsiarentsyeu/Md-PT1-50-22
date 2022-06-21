eb52a5374adf60cd038da665bff1fe20f91ed4fb
DobruyDzen <100238846+DobruyDzen@users.noreply.github.com>
2022-06-21 11:34:57 +0300

    Create Task5.py

---
+++ Tasks/Terpitsky/Task5/Task5.py
@@ -0,0 +1,24 @@
+def list_summ(my_list, ready_list = [], ready_summ = [0]):
+    for i in my_list:
+        if type(i) == list:
+            list_summ([*i])
+        else:
+            ready_summ[0] += i
+            ready_list.append(i)
+    return f'sum of all elements: {ready_summ[0]}'
+
+def fib(n, ready_list = [0,1]):
+    if n == 0:
+        return
+    ready_list.append(ready_list[-1] + ready_list[-2])
+    fib(n - 1)
+    return ready_list[:-2]
+
+
+print(list_summ([1,1,[],1,4,[1,1],1,1,[1,[[[[1]]]],1],1,1,1,3,[[[[]]]],2,[[2]]]))
+
+print(fib(56))
+
+
+
+
