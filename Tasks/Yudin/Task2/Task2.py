4baad1a00d687f4405d55a5ac6710286c1f5511a
YaroslavYudin <104415042+YaroslavYudin@users.noreply.github.com>
2022-09-20 19:55:49 +0300

    Add Task2

---
+++ Tasks/Yudin/Task2/Task2.py
@@ -0,0 +1,75 @@
+import datetime
+
+choose_time = input('Please, choose time: \n enter "1" for choose current time or "2" to enter the time manually - ')
+
+if choose_time == '1':
+    current_time = datetime.datetime.now()
+    time_hour = current_time.hour
+    time_minute = current_time.minute
+
+elif choose_time == '2':
+    enter_time = input('Please enter time in format hh:mm - ')
+    if len(enter_time) != 5:
+        print('Incorrect time format')
+        quit()
+    elif enter_time[2] != ':':
+        print('Incorrect sign for dividing hours and minutes')
+        quit()
+    elif not enter_time[:2].isdigit() or not enter_time[3:].isdigit():
+        print('Wrong. Please enter time in numbers in format hh:mm')
+        quit()
+    time_hour = int(enter_time.split(':')[0])
+    time_minute = int(enter_time.split(':')[1])
+    if time_hour > 24:
+        print('Incorrect hours value')
+        quit()
+    elif time_hour == 24 and time_minute > 0:
+        print('Incorrect minutes value for 00 hours')
+        quit()
+    elif time_minute > 59:
+        print('Incorrect minutes value')
+        quit()
+else:
+    print('Please correct choose working mode.')
+    quit()
+
+if 12 < time_hour:
+    time_hour -= 12
+
+dict_mm = {1:('одна минута', 'одной минуты'), 2:('две минуты', 'двух минут'), 3:('три минуты', 'трех минут'),
+4:('четыре минуты', 'четырех минут'), 5:('пять минут', 'пяти минут'), 6:('шесть минут', 'шести минут'),
+7:('семь минут', 'семи минут'), 8:('восемь минут', 'восьми минут'), 9:('девять минут', 'девяти минут'),
+10:('десять минут', 'десяти минут'), 11:('одиннадцать минут', 'одиннадцати минут'),
+12:('двенадцать минут', 'двенадцати минут'), 13:('тринадцать минут', 'тринадцати минут'),
+14:('четырнадцать минут', 'четырнадцати минут'), 15:('пятнадцать минут', 'пятнадцати минут'),
+16:('шестнадцать минут', 'шестнадцати минут'), 17:('семнадцать минут', 'семнадцати минут'),
+18:('восемнадцать минут', 'восемнадцати минут'), 19:('девятнадцать минут', 'девятнадцати минут'),
+20:('двадцать минут', 'двадцать'), 30:('тридцать минут', 'тридцать'), 40:('сорок минут', 'сорок')}
+
+dict_hh = {1:('час', 'первого'), 2:('два часа', 'второго'), 3:('три часа', 'третьего'), 4:('четыре часа', 'четвертого'),
+5:('пять часов', 'пятого'), 6:('шесть часов', 'шестого'), 7:('семь часов', 'седьмого'), 8:('восемь часов', 'восьмого'),
+9:('девять часов', 'девятого'), 10:('десять часов', 'десятого'), 11:('одиннадцать часов', 'одиннадцатого'),
+12:('двенадцать часов', 'двенадцатого'), 13:('час', 'первого')}
+
+if time_minute == 0:
+    if time_hour == 0 or time_hour == 24:
+        print('полночь')
+    elif time_hour == 12:
+        print('полдень')
+    else:
+        print(dict_hh[time_hour][0], 'ровно')
+
+elif time_minute <= 20:
+    if time_hour == 12:
+        print(dict_mm[time_minute][0], dict_hh[time_hour - 11][1])
+        quit()
+    print(dict_mm[time_minute][0], dict_hh[time_hour + 1][1])
+
+elif 20 < time_minute < 45 and time_minute != 30:
+    print(dict_mm[time_minute // 10 * 10][1], dict_mm[time_minute % 10][0], dict_hh[time_hour + 1][1])
+
+elif time_minute == 30:
+    print('половина', dict_hh[time_hour + 1][1])
+
+elif time_minute >= 45:
+    print('без', dict_mm[60 - time_minute][1], dict_hh[time_hour + 1][0])
